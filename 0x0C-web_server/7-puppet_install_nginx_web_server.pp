d77586b8e450a86003dcc27c8fa8df02f748a685
arizzen <a1.arizzen@gmail.com>
2023-06-13 16:55:44 +0100

    kk

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,35 @@
-# Installs a Nginx server
+# Manifest that configures NGINX
+exec { 'exec_0':
+  command => 'sudo sudo apt-get update -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+exec { 'exec_1':
+  require => Exec['exec_0'],
+  command => 'sudo apt-get install nginx -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_2':
+  require => Exec['exec_1'],
+  command => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
+}
+
+exec { 'exec_3':
+  require     => Exec['exec_2'],
+  environment => ['GG=google.com permanent'],
+  command     => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me $GG;/" /etc/nginx/sites-enabled/default',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0,1]
+}
+
+exec { 'exec_4':
+  require => Exec['exec_3'],
+  command => 'sudo service nginx start',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
